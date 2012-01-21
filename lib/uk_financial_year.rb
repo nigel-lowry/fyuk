592cb94e4f879befff2f975114a12a5dfca6a569
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2012-01-21 22:19:56 +0000

    Added a comment about HMRC format

--- lib/uk_financial_year.rb
+++ lib/uk_financial_year.rb
@@ -67,7 +67,8 @@ class UkFinancialYear
     self.first_day.next_year == other_financial_year.first_day || self.first_day.prev_year == other_financial_year.first_day
   end
 
-  # returns string representation of the financial year in the form '2000/01'
+  # returns string representation of the financial year in the form '2000/01'.
+  # This is the form HMRC use.
   # @return [String] representation in the form of the first year as four
   # digits, a '/', then the last year as two digits
   def to_s

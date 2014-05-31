45078f6766927742c1afa7595c7955420aeaed19
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-31 16:46:51 +0100

    Remove parens

--- spec/uk_financial_year_spec.rb
+++ spec/uk_financial_year_spec.rb
@@ -4,11 +4,11 @@ describe UkFinancialYear do
 
   subject { UkFinancialYear.from_s '2012/13' }
 
-  its(:first_day) { is_expected.to eq('6 Apr 2012'.to_date) }
-  its(:last_day) { is_expected.to eq('5 Apr 2013'.to_date) }
-  its(:next) { is_expected.to eq(UkFinancialYear.from_s '2013/14') }
-  its(:previous) { is_expected.to eq(UkFinancialYear.from_s '2011/12') }
-  its(:to_s) { is_expected.to eq('2012/13') }
+  its(:first_day) { is_expected.to eq '6 Apr 2012'.to_date }
+  its(:last_day) { is_expected.to eq '5 Apr 2013'.to_date }
+  its(:next) { is_expected.to eq UkFinancialYear.from_s '2013/14' }
+  its(:previous) { is_expected.to eq UkFinancialYear.from_s '2011/12' }
+  its(:to_s) { is_expected.to eq '2012/13' }
 
   specify { expect(UkFinancialYear.new '6 Apr 2011'.to_date).to eq(UkFinancialYear.new '7 Apr 2011'.to_date) }
   specify { expect(UkFinancialYear.new '5 Apr 2011'.to_date).to_not eq(UkFinancialYear.new '6 Apr 2011'.to_date) }
@@ -16,22 +16,22 @@ describe UkFinancialYear do
   specify { expect(UkFinancialYear.new '5 Apr 2011'.to_date).to be < UkFinancialYear.new('6 Apr 2011'.to_date) }
 
   describe "#to_s" do
-    specify { expect(UkFinancialYear.new('7 Apr 2011'.to_date).to_s).to eq('2011/12') }
-    specify { expect(UkFinancialYear.new('7 Apr 1999'.to_date).to_s).to eq('1999/00') }
+    specify { expect(UkFinancialYear.new('7 Apr 2011'.to_date).to_s).to eq '2011/12' }
+    specify { expect(UkFinancialYear.new('7 Apr 1999'.to_date).to_s).to eq '1999/00' }
   end
 
   describe "#first_day" do
-    specify { expect(UkFinancialYear.new('4 Apr 2011'.to_date).first_day).to eq('6 Apr 2010'.to_date) }
-    specify { expect(UkFinancialYear.new('5 Apr 2011'.to_date).first_day).to eq('6 Apr 2010'.to_date) }
-    specify { expect(UkFinancialYear.new('6 Apr 2011'.to_date).first_day).to eq('6 Apr 2011'.to_date) }
-    specify { expect(UkFinancialYear.new('7 Apr 2011'.to_date).first_day).to eq('6 Apr 2011'.to_date) }
+    specify { expect(UkFinancialYear.new('4 Apr 2011'.to_date).first_day).to eq '6 Apr 2010'.to_date }
+    specify { expect(UkFinancialYear.new('5 Apr 2011'.to_date).first_day).to eq '6 Apr 2010'.to_date }
+    specify { expect(UkFinancialYear.new('6 Apr 2011'.to_date).first_day).to eq '6 Apr 2011'.to_date }
+    specify { expect(UkFinancialYear.new('7 Apr 2011'.to_date).first_day).to eq '6 Apr 2011'.to_date }
   end
 
   describe "#last_day" do
-    specify { expect(UkFinancialYear.new('4 Apr 2011'.to_date).last_day).to eq('5 Apr 2011'.to_date) }
-    specify { expect(UkFinancialYear.new('5 Apr 2011'.to_date).last_day).to eq('5 Apr 2011'.to_date) }
-    specify { expect(UkFinancialYear.new('6 Apr 2011'.to_date).last_day).to eq('5 Apr 2012'.to_date) }
-    specify { expect(UkFinancialYear.new('7 Apr 2011'.to_date).last_day).to eq('5 Apr 2012'.to_date) }
+    specify { expect(UkFinancialYear.new('4 Apr 2011'.to_date).last_day).to eq '5 Apr 2011'.to_date }
+    specify { expect(UkFinancialYear.new('5 Apr 2011'.to_date).last_day).to eq '5 Apr 2011'.to_date }
+    specify { expect(UkFinancialYear.new('6 Apr 2011'.to_date).last_day).to eq '5 Apr 2012'.to_date }
+    specify { expect(UkFinancialYear.new('7 Apr 2011'.to_date).last_day).to eq '5 Apr 2012'.to_date }
   end
 
   describe "#.include?" do
@@ -44,10 +44,10 @@ describe UkFinancialYear do
   end
 
   describe "#from_s" do
-    specify { expect(UkFinancialYear.from_s('1997/98').to_s).to eq('1997/98') }
-    specify { expect(UkFinancialYear.from_s('1998/99').to_s).to eq('1998/99') }
-    specify { expect(UkFinancialYear.from_s('1999/00').to_s).to eq('1999/00') }
-    specify { expect(UkFinancialYear.from_s('2000/01').to_s).to eq('2000/01') }
+    specify { expect(UkFinancialYear.from_s('1997/98').to_s).to eq '1997/98' }
+    specify { expect(UkFinancialYear.from_s('1998/99').to_s).to eq '1998/99' }
+    specify { expect(UkFinancialYear.from_s('1999/00').to_s).to eq '1999/00' }
+    specify { expect(UkFinancialYear.from_s('2000/01').to_s).to eq '2000/01' }
 
     it "raises error if years not consecutive" do
       expect {
@@ -80,16 +80,16 @@ describe UkFinancialYear do
   describe "#before? and #after?" do
     subject { UkFinancialYear.from_s '2011/12' }
 
-    it { is_expected.to be_before UkFinancialYear.from_s('2012/13') }
+    it { is_expected.to be_before UkFinancialYear.from_s '2012/13' }
     it { is_expected.to be_before UkFinancialYear.from_s('2012/13').first_day }
-    it { is_expected.to be_after UkFinancialYear.from_s('2010/11') }
+    it { is_expected.to be_after UkFinancialYear.from_s '2010/11' }
     it { is_expected.to be_after UkFinancialYear.from_s('2010/11').last_day }
   end
 
   describe "creation without date" do
     it "is the current financial year" do
       Timecop.freeze('5 Jul 2010'.to_date) do
-        expect(UkFinancialYear.new).to eq(UkFinancialYear.from_s '2010/11')
+        expect(UkFinancialYear.new).to eq UkFinancialYear.from_s '2010/11'
       end
     end
   end
@@ -115,8 +115,8 @@ describe UkFinancialYear do
       )
     end
 
-    specify { expect(subject.period_before('6 Apr 2012'.to_date)).to eq('6 Apr 2012'.to_date...'6 Apr 2012'.to_date) }
-    specify { expect(subject.period_before('7 Apr 2012'.to_date)).to eq('6 Apr 2012'.to_date...'7 Apr 2012'.to_date) }
-    specify { expect(subject.period_before('8 Apr 2012'.to_date)).to eq('6 Apr 2012'.to_date...'8 Apr 2012'.to_date) }
+    specify { expect(subject.period_before('6 Apr 2012'.to_date)).to eq '6 Apr 2012'.to_date...'6 Apr 2012'.to_date }
+    specify { expect(subject.period_before('7 Apr 2012'.to_date)).to eq '6 Apr 2012'.to_date...'7 Apr 2012'.to_date }
+    specify { expect(subject.period_before('8 Apr 2012'.to_date)).to eq '6 Apr 2012'.to_date...'8 Apr 2012'.to_date }
   end
 end

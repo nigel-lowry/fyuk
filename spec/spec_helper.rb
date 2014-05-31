caded89715a1e287f9abe28269d9554542f2c12d
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-31 16:32:46 +0100

    Require in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
+require 'rspec/its'
 require 'uk_financial_year'
 require 'timecop'
 
